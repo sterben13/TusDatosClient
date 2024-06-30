86f68205968c009ab1ecfbbae53a6396920cbffc
sterben93 <ivan.dev.job@gmail.com>
2024-06-30 15:54:57 -0600

    feat: init commit

---
+++ src/main/java/com/tusdatos/TusDatosApplication.java
@@ -0,0 +1,13 @@
+package com.tusdatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusDatosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TusDatosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tusdatos/TusDatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusdatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusDatosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
